48fdd650b859d581f5d238a94c5ff4ae2f990d4f
youssef <programming20062024@gmail.com>
2025-10-24 15:27:52 +0300

    Update PlayerAudio.h

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -1,16 +1,21 @@
+
+
 #pragma once
 #include <JuceHeader.h>
 
 class PlayerAudio
 {
 public:
+    // Constructor and Destructor
     PlayerAudio();
-    ~PlayerAudio();
+    ~PlayerAudio(); // مهم لإدارة الذاكرة
 
+    // --- دوال الصوت الأساسية ---
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
     void releaseResources();
 
+    // --- دوال التحكم في التشغيل (مدمجة من الكودين) ---
     void loadFile(const juce::File& file);
     void play();
     void pause();
@@ -20,9 +25,19 @@ public:
     void goToEnd();
     void setGain(float newGain);
 
+    // الدالة دي من نسختك أنت
+    void setPosition(double position);
+
+    // ودي كمان، أهم إضافة بتاعتك (زرار اللوب)
+    void setLooping(bool shouldLoop);
+
+    void toggleMute();
+
 private:
     juce::AudioFormatManager formatManager;
     std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
     juce::AudioTransportSource transportSource;
+
+    bool isMuted = false;       // متغير عشان نعرف حالة الكتم الحالية
+    float lastKnownVolume = 0.5f; // متغير عشان نحفظ آخر مستوى صوت قبل الكتم
 };
-#pragma once
