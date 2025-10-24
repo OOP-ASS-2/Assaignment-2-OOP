179bb7232965a378735595cc0dad0d039be361d1
youssef <programming20062024@gmail.com>
2025-10-24 15:26:15 +0300

    Update MainComponent.h

--- MainComponent.h
+++ MainComponent.h
@@ -1,22 +1,42 @@
-﻿#pragma once
+
+#pragma once
+
 #include <JuceHeader.h>
-#include "PlayerGUI.h"
+#include "PlayerAudio.h" // لازم نعمل include للملف ده عشان يعرف يعني إيه PlayerAudio
+#include "PlayerGUI.h"   // ولازم ده عشان يعرف يعني إيه PlayerGUI
 
 //==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
 class MainComponent : public juce::AudioAppComponent
 {
 public:
+    //==============================================================================
     MainComponent();
     ~MainComponent() override;
 
+    //==============================================================================
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
 
-    void paint(juce::Graphics&) override;
+    //==============================================================================
+    void paint(juce::Graphics& g) override;
     void resized() override;
 
 private:
-    PlayerGUI playerGUI; // كائن الواجهة الخاصة بالمشغل
+    //==============================================================================
+    // Your private member variables go here...
+
+    // هنا بنعمل أوبجكت من كلاس الصوت عشان يشيل كل اللوجيك بتاع تشغيل الملفات
+    PlayerAudio playerAudio;
+
+    // وهنا بنعمل أوبجكت الواجهة، وبنديله الأوبجكت بتاع الصوت (playerAudio)
+    // عشان لما ندوس على زرار في الواجهة، يعرف يكلم مين عشان يشغل أو يوقف الصوت
+    PlayerGUI   playerGUI{ playerAudio };
+
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
