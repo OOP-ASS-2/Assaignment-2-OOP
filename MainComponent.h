57dbb216a020809ce4ab80bb93126b37f873cf84
Khalid Ahmed <95830957+Khalid-2005@users.noreply.github.com>
2025-10-24 15:56:35 +0300

    the new MainComponent header

--- MainComponent.h
+++ MainComponent.h
@@ -1,42 +1,16 @@
-
-#pragma once
-
+#pragma once // MainComponent.h
 #include <JuceHeader.h>
-#include "PlayerAudio.h" // لازم نعمل include للملف ده عشان يعرف يعني إيه PlayerAudio
-#include "PlayerGUI.h"   // ولازم ده عشان يعرف يعني إيه PlayerGUI
-
-//==============================================================================
-/*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
-*/
+#include "PlayerGUI.h"
 class MainComponent : public juce::AudioAppComponent
 {
 public:
-    //==============================================================================
-    MainComponent();
-    ~MainComponent() override;
-
-    //==============================================================================
-    void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
-    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
-    void releaseResources() override;
-
-    //==============================================================================
-    void paint(juce::Graphics& g) override;
-    void resized() override;
-
+	MainComponent();
+	~MainComponent() override;
+	void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+	void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
+	void releaseResources() override;
+	void resized() override;
 private:
-    //==============================================================================
-    // Your private member variables go here...
-
-    // هنا بنعمل أوبجكت من كلاس الصوت عشان يشيل كل اللوجيك بتاع تشغيل الملفات
-    PlayerAudio playerAudio;
-
-    // وهنا بنعمل أوبجكت الواجهة، وبنديله الأوبجكت بتاع الصوت (playerAudio)
-    // عشان لما ندوس على زرار في الواجهة، يعرف يكلم مين عشان يشغل أو يوقف الصوت
-    PlayerGUI   playerGUI{ playerAudio };
-
-
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
+	PlayerGUI player1;
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
