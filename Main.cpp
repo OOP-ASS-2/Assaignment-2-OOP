a03e4167de9bedfcd814092d5c38cadbf7a0f607
Khalid Ahmed <95830957+Khalid-2005@users.noreply.github.com>
2025-11-07 10:49:14 +0200

    the new main

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,7 @@
-//new
 #include <JuceHeader.h>
 #include "MainComponent.h"
 
-//==============================================================================
-// Application Class
-//==============================================================================
+
 class AudioMixerApp : public juce::JUCEApplication
 {
 public:
@@ -12,13 +9,12 @@ public:
     const juce::String getApplicationVersion() override { return "1.0"; }
     bool moreThanOneInstanceAllowed() override { return true; }
 
-    // عند بدء التشغيل
+
     void initialise(const juce::String&) override
     {
         mainWindow.reset(new MainWindow(getApplicationName(), new MainComponent(), *this));
     }
 
-    // عند إغلاق التطبيق
     void shutdown() override
     {
         mainWindow = nullptr;
@@ -27,8 +23,7 @@ public:
     void systemRequestedQuit() override { quit(); }
 
 private:
-    //==============================================================================
-    // Main Window Class
+
     class MainWindow : public juce::DocumentWindow
     {
     public:
@@ -39,30 +34,30 @@ private:
                 DocumentWindow::allButtons),
             app(a)
         {
-            // استخدم شريط العنوان الأصلي بتاع الويندوز
+
             setUsingNativeTitleBar(true);
 
-            // خلي النص في النص باللون الأبيض
+
             setTitleBarTextCentred(true);
             setColour(DocumentWindow::textColourId, juce::Colours::white);
 
-            // حط المحتوى الرئيسي (MainComponent)
+
             setContentOwned(c, true);
 
-            // خليه قابل للتكبير والتصغير
+
             setResizable(true, true);
 
-            // خلي حجم النافذة مناسب والعنوان ما يتقطعش
+
             centreWithSize(900, 550);
 
-            // خليه مرئي بعد ما يتجهز
+
             setVisible(true);
 
-            // اسم العنوان فعلياً
+
             setName(" JUCE Audio Mixer");
         }
 
-        // لما المستخدم يقفل البرنامج
+
         void closeButtonPressed() override
         {
             juce::JUCEApplication::getInstance()->systemRequestedQuit();
@@ -75,8 +70,5 @@ private:
     std::unique_ptr<MainWindow> mainWindow;
 };
 
-//==============================================================================
-// Entry point
-//==============================================================================
-START_JUCE_APPLICATION(AudioMixerApp)
 
+START_JUCE_APPLICATION(AudioMixerApp)
