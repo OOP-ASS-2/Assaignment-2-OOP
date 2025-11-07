64fd29c1ca8995f792f5205d8b28a6b223ce7b8f
Khalid Ahmed <95830957+Khalid-2005@users.noreply.github.com>
2025-11-07 10:53:48 +0200

    Add loadURL method to WaveformDisplay class
    Added loadURL method to load audio files into the waveform display.

--- WaveformDisplay.h
+++ WaveformDisplay.h
@@ -1,25 +1,31 @@
 #pragma once
 
 #include <JuceHeader.h>
-#include "PlayerAudio.h" // نحتاج هذا للوصول إلى PlayerAudio
+#include "PlayerAudio.h" 
 
 class WaveformDisplay : public juce::Component,
     public juce::Timer
 {
 public:
-    // نمرر له محرك الصوت بالكامل
+
     WaveformDisplay(PlayerAudio& audioPlayer);
     ~WaveformDisplay() override;
 
     void paint(juce::Graphics& g) override;
     void resized() override;
 
-    // هذه الدالة سيتم استدعاؤها بشكل متكرر لتحديث الرسم
+
     void timerCallback() override;
 
-private:
-    PlayerAudio& playerAudio; // مرجع لمحرك الصوت
 
-    // دالة لرسم مؤشر التشغيل
+    void loadURL(const juce::File& file) { playerAudio.getThumbnail().setSource(new juce::FileInputSource(file)); }
+
+
+private:
+    PlayerAudio& playerAudio;
+
+
     void drawPlaybackPosition(juce::Graphics& g);
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(WaveformDisplay)
 };
